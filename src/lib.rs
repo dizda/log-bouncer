04ae348a4c140b2223bc653883413c03c414c810
Jonathan Dizdarevic <dizzda@gmail.com>
2021-09-06 18:38:56 +0700

    chore: cargo fix

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,10 @@ mod publisher;
 mod watcher;
 
 use crate::output::stdout::StdOut;
-use crate::output::OutputAdapter;
 use crate::publisher::Publisher;
 use crate::watcher::Watcher;
-use logwatcher::{LogWatcher, LogWatcherAction};
 use std::error::Error;
-use tokio::runtime::Runtime;
 use tokio::sync::mpsc;
-use tokio::sync::mpsc::Receiver;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::EnvFilter;
 
@@ -23,9 +19,9 @@ pub async fn run() -> Result<(), Box<dyn Error>> {
     let log = tracing_subscriber::fmt().with_env_filter(EnvFilter::from_default_env());
     log.finish().init();
 
-    let (tx, mut rx) = mpsc::channel::<String>(1);
+    let (tx, rx) = mpsc::channel::<String>(1);
 
-    let mut watcher = Watcher::new(FILE, tx)?;
+    let watcher = Watcher::new(FILE, tx)?;
     watcher.work();
 
     Publisher::new(StdOut {}, rx).publish().await;
