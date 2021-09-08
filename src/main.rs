3378c934d4bde3c28b7ba70cce6ccf4dfb418f3e
Jonathan Dizdarevic <dizzda@gmail.com>
2021-09-08 18:29:21 +0700

    refactor: opt in a sub mod

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use file_trailer::parse;
 use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    file_trailer::run().await
+    file_trailer::run(parse()).await
 }
---
+++ src/opt.rs
@@ -0,0 +1,44 @@
+use clap::{AppSettings, Clap};
+use std::path::PathBuf;
+
+/// # File-trailer
+///
+/// File trailer will listen a log file then:
+///
+///  - publish any new message to AMQP
+///  - rotate logs automatically
+///
+#[derive(Debug, clap::Clap, Clone)]
+#[clap(name = "file-trailer")]
+pub struct Opt {
+    /// Override the config file
+    #[clap(parse(from_os_str), short, long)]
+    pub file: PathBuf,
+
+    /// If the filesize go beyond that value, the file will get rotated
+    #[clap(short, long, default_value = "1000")]
+    pub max_filesize: u64,
+
+    /// Check if the file needs to be rotated
+    /// value in seconds
+    #[clap(short, long, default_value = "5")]
+    pub rotate_file_interval: u64,
+
+    /// Rotated files will have a date on their filenames,
+    /// can change the current structure `%Y-%m-%d-%H-%M-%S`
+    pub date_format: Option<String>,
+
+    /// Uri of the AMQP server to publish to
+    #[clap(long, default_value = "amqp://guest:guest@127.0.0.1:5672/%2f")]
+    pub amqp_uri: String,
+
+    #[clap(long)]
+    pub amqp_exchange: String,
+
+    #[clap(long)]
+    pub amqp_routing_key: String,
+}
+
+pub fn parse() -> Opt {
+    Opt::parse()
+}
