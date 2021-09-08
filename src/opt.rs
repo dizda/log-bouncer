4a796cb92653873be2affabbb36b294f7a417f0e
Jonathan Dizdarevic <dizzda@gmail.com>
2021-09-08 18:41:32 +0700

    fix: can use env vars

--- src/opt.rs
+++ src/opt.rs
@@ -12,30 +12,31 @@ use std::path::PathBuf;
 #[clap(name = "file-trailer")]
 pub struct Opt {
     /// Override the config file
-    #[clap(parse(from_os_str), short, long)]
+    #[clap(parse(from_os_str), short, long, env)]
     pub file: PathBuf,
 
     /// If the filesize go beyond that value, the file will get rotated
-    #[clap(short, long, default_value = "1000")]
+    #[clap(short, long, default_value = "1000", env)]
     pub max_filesize: u64,
 
     /// Check if the file needs to be rotated
     /// value in seconds
-    #[clap(short, long, default_value = "5")]
+    #[clap(short, long, default_value = "5", env)]
     pub rotate_file_interval: u64,
 
     /// Rotated files will have a date on their filenames,
     /// can change the current structure `%Y-%m-%d-%H-%M-%S`
+    #[clap(short, long)]
     pub date_format: Option<String>,
 
     /// Uri of the AMQP server to publish to
-    #[clap(long, default_value = "amqp://guest:guest@127.0.0.1:5672/%2f")]
+    #[clap(long, default_value = "amqp://guest:guest@127.0.0.1:5672/%2f", env)]
     pub amqp_uri: String,
 
-    #[clap(long)]
+    #[clap(long, env)]
     pub amqp_exchange: String,
 
-    #[clap(long)]
+    #[clap(long, env)]
     pub amqp_routing_key: String,
 }
 
